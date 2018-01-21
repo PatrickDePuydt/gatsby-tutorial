ec05a765e452e3f61371fbdac145c8aa7071742b
Patrick DePuydt <patrickdepuydt@Patricks-MacBook-Air.local>
2018-01-21 16:20:08 -0500

    Add state component

--- tutorial-part-one/src/pages/index.js
+++ tutorial-part-one/src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{color: `dodgerblue`}}>
+	<h1>Hello World!</h1>
+	<p>Gatsby on a Sunday Afternoon</p>
+	<img src="https://source.unsplash.com/random/400x200" alt="" />
+	<br />
+    <div>
+      <Link to="/page-2/">Page 2</Link>
+    </div>
+    <div>
+      <Link to="/Counter/">Counter</Link>
+    </div>
+</div>
---
+++ tutorial-part-one/src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => 
+<div>
+	<h1>Page 2</h1>
+
+    <div>
+      <Link to="/">Home</Link>
+    </div>
+
+</div>
